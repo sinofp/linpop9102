6dea21b297e5c4f0a5a283fd85a222ed541c6194
unv <achinese@live.cn>
2019-08-30 14:17:02 +0800

    add qt example

---
+++ untitled/main.cpp
@@ -0,0 +1,13 @@
+#include "login.h"
+#include <QApplication>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
+    Login w;
+    w.show();
+
+    return a.exec();
+}
